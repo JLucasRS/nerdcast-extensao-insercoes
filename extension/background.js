fa39cf873944f179ed0c2066aeec7cfe0f8cf0be
JLucasRS <jlucasrs2012@gmail.com>
2021-10-24 08:03:31 -0300

    Adicionado background.js, valores iniciais agoram são definidos corretamente.

---
+++ extension/background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({
+        showGallery: true,
+        useSound: true,
+        showInsertions: true,
+        skipEmails: true
+    });
+});
